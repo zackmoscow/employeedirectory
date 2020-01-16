fbda9eceb5cbb91ebd3f99928dcac68b2e0e332d
Zack Moscow <zmoscow@gmail.com>
2020-01-15 18:56:17 -0600

    minor tweak for backend deployment

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const path = require("path");
-
 const PORT = process.env.PORT || 3000;
-
+const publicPath = path.join(__dirname, '..', 'public');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public');
-
 app.use(express.static(publicPath));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static("public"));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
