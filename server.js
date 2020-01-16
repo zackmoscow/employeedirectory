3a420b001dc8e2cd317e65ad49fae5a7564c0b86
Zack Moscow <zmoscow@gmail.com>
2020-01-15 20:29:10 -0600

    another try for heroku deployment

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
