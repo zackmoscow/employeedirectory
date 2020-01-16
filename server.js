bac593667fe5ad72fa5ca089b817a764e3ea6f82
Zack Moscow <zmoscow@gmail.com>
2020-01-15 19:01:16 -0600

    still trying to get heroku to work

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'public');
 const app = express();
 
-app.use(express.static(publicPath));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
- });
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
